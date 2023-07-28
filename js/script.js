01e28a8874b71e2df7a5b16e1b05772de341af4e
norizmi <naufalnaufal841@gmail.com>
2023-07-28 10:49:33 +0700

    added fixed navbar,scroll effect ,and js script

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const navHome = document.getElementById("nav-home");
+const scrollTop = document.querySelector(".scroll");
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 10 ) {
+        navHome.classList.add("nav-home");
+        navHome.classList.remove("nav-home-transparent");
+        scrollTop.classList.add("scroll-top");
+
+    } 
+    else {
+        navHome.classList.add("nav-home-transparent");
+        navHome.classList.remove("nav-home");
+        scrollTop.classList.remove("scroll-top");
+    }
+}) 
