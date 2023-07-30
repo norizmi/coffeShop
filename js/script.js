f998257887187794da1807597d55b874d5e625af
norizmi <naufalnaufal841@gmail.com>
2023-07-30 17:48:33 +0700

    added slide eff in main html  scale eff in menu

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ window.addEventListener("scroll", function() {
         scrollTop.classList.remove("scroll-top");
     }
 }) 
+
+
+const contentWrapper = document.querySelectorAll(".content-wrapper");
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("slide-right", entry.isIntersecting);
+        entry.isIntersecting ? observer.unobserve(entry.target) : false;
+    })
+})
+
+contentWrapper.forEach(content => {
+    observer.observe(content);
+})
+
+
