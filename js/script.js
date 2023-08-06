69d7e33f73d1d9573ef32ad8925c7feb52da44d6
norizmi <naufalnaufal841@gmail.com>
2023-08-06 21:34:33 +0700

    added products filter(not fixed)

--- js/script.js
+++ js/script.js
@@ -27,4 +27,48 @@ contentWrapper.forEach(content => {
     observer.observe(content);
 })
 
+function getQueryParams() {
+    let query = window.location.search;
+    let params = new URLSearchParams(query);
+    let url = params.get("q");
 
+    return url;
+}
+
+
+function coffee() {
+    const coffeeList = document.querySelectorAll(".coffee");
+    coffeeList.forEach(coff => {
+        if(getQueryParams() === "coffee") {
+            coff.style.display = "block";
+        } else {
+            coff.style.display = "none";
+        }
+    })
+}
+
+function nonCoffee() {
+    const nonCoffee = document.querySelectorAll(".non-coffee");
+    nonCoffee.forEach(nonCoff => {
+        if(getQueryParams() === "non-coffee") {
+            nonCoff.style.display = "block";
+        } else {
+            nonCoff.style.display = "none";
+        }
+    })
+}
+
+function tea() {
+    const tea = document.querySelectorAll(".tea");
+    tea.forEach(teaList => {
+        if(getQueryParams() === "tea") {
+            teaList.style.display = "block";
+        } else {
+            teaList.style.display = "none";
+        }
+    })
+}
+
+tea()
+nonCoffee()
+coffee();
