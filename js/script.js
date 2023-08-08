09e1ad4ee121c68b1c59c237107f65147be43f53
norizmi <naufalnaufal841@gmail.com>
2023-08-08 13:08:25 +0700

    fixed menu.html

--- js/script.js
+++ js/script.js
@@ -24,67 +24,73 @@ contentWrapper.forEach((content) => {
   observer.observe(content);
 });
 
-function getQueryParams() {
-  let query = window.location.search;
-  let params = new URLSearchParams(query);
-  let url = params.get("q");
+// function getQueryParams() {
+//   let query = window.location.search;
+//   let params = new URLSearchParams(query);
+//   let url = params.get("q");
 
-  return url;
-}
+//   return url;
+// }
 
-function defaultCondition() {
-  let query = window.location.search;
-  const specificMenu = document.querySelectorAll("specific-menu");
-  let params = new URLSearchParams(query);
-  let url = params.has("q");
+// function defaultCondition() {
+//   let query = window.location.search;
+//   const specificMenu = document.querySelectorAll("specific-menu");
+//   let params = new URLSearchParams(query);
+//   let url = params.has("q");
 
-  switch (url) {
-    case false:
-      specificMenu.style.display = "block";
-      break;
-    default:
-      tea();
-      nonCoffee();
-      coffee();
-  }
+//   switch (url) {
+//     case false:
+//       specificMenu.style.display = "block";
+//       break;
+//     default:
+//       tea();
+//       nonCoffee();
+//       coffee();
+//   }
 
-  return specificMenu;
-}
+//   return specificMenu;
+// }
 
-function coffee() {
-  const coffeeList = document.querySelectorAll(".coffee");
-  coffeeList.forEach((coff) => {
-    if (getQueryParams() === "coffee") {
-      coff.style.display = "block";
-    } else {
-      coff.style.display = "none";
-    }
-  });
-}
+// function coffee() {
+//   const coffeeList = document.querySelectorAll(".coffee");
+//   coffeeList.forEach((coff) => {
+//     if (getQueryParams() === "coffee") {
+//       coff.style.display = "block";
+//     } else {
+//       coff.style.display = "none";
+//     }
+//   });
+// }
 
-function nonCoffee() {
-  const nonCoffee = document.querySelectorAll(".non-coffee");
-  nonCoffee.forEach((nonCoff) => {
-    if (getQueryParams() === "non-coffee") {
-      nonCoff.style.display = "block";
-    } else {
-      nonCoff.style.display = "none";
-    }
-  });
-}
+// function nonCoffee() {
+//   const nonCoffee = document.querySelectorAll(".non-coffee");
+//   nonCoffee.forEach((nonCoff) => {
+//     if (getQueryParams() === "non-coffee") {
+//       nonCoff.style.display = "block";
+//     } else {
+//       nonCoff.style.display = "none";
+//     }
+//   });
+// }
 
-function tea() {
-  const tea = document.querySelectorAll(".tea");
-  tea.forEach((teaList) => {
-    if (getQueryParams() === "tea") {
-      teaList.style.display = "block";
-    } else {
-      teaList.style.display = "none";
-    }
-  });
-}
+// function tea() {
+//   const tea = document.querySelectorAll(".tea");
+//   tea.forEach((teaList) => {
+//     if (getQueryParams() === "tea") {
+//       teaList.style.display = "block";
+//     } else {
+//       teaList.style.display = "none";
+//     }
+//   });
+// }
 
-defaultCondition();
+// defaultCondition();
+
+//Active Bar Header
+const path = location.pathname;
+console.log(path)
+console.log(window.location)
+
 
 // Accordion
 // const accordion = document.querySelectorAll(".accordion");
