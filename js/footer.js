262ed1c9ec405e7af9516082e354a76e077e3b79
norizmi <naufalnaufal841@gmail.com>
2023-08-09 14:46:54 +0700

    fixed accodion in mobile ver

--- js/footer.js
+++ js/footer.js
@@ -1,15 +1,16 @@
-const accordion = document.querySelectorAll(".accordion") 
+const accordion = document.querySelectorAll(".accordion");
+accordion.forEach((acc) => {
+  acc.addEventListener("click", () => {
+    const sub = acc.children;
+    const panel = sub[2];
+    const arrow = sub[1];
 
-accordion.forEach(acc => {
-    acc.addEventListener("click", function() {
-     const panel = this.nextElementSibling;
-     const arrow = acc.querySelector("img")
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-        arrow.style.transform = "rotate(180deg)";
-      } else {
-        panel.style.display = "block";
-        arrow.style.transform = "rotate(90deg)";
-      }
-    })
-})
+    if(panel.style.display === "block") {
+      panel.style.display = "none";
+      arrow.style.transform = "rotate(180deg)";
+    } else {
+      panel.style.display = "block";
+      arrow.style.transform = "rotate(90deg)";
+    }
+  });
+});
