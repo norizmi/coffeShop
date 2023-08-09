698c947591e5c22f63ef7bbfa7789fcc3359f983
norizmi <naufalnaufal841@gmail.com>
2023-08-09 10:54:09 +0700

    rename findUs.html

--- js/header.js
+++ js/header.js
@@ -1,10 +1,10 @@
 const path = window.location.pathname;
-const url = path.slice(6);
+const url = path.slice(-9);
 const headerBar = document.querySelectorAll("ul li a[href]");
 
 headerBar.forEach(bar => {
         let link = bar.href;
-        if(link.slice(27) === url) {
+        if(link.slice(-9) === url) {
             bar.classList.toggle("active-header");
         }
 })
